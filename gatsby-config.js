69d184b7f4d6b2181de283a46a97d3f20cf1fbf6
Timothy Renner <lintel@santiam.local>
2020-09-27 22:13:27 -0700

    working Row component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rythm-a-ning`,
+    description: `Kick major butt with these time-tested beats.`,
+    author: `@TIMRENNER`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,92 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
-import Open from "../../assets/Open.svg"
-import Bell from "../../assets/Bell.svg"
-import Closed from "../../assets/Closed.svg"
-import Space from "../../assets/Space.svg"
+import Row from "../components/Row"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <h1>Soli</h1>
     <SEO title="Home" />
-    <h1>Hello Dununs</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div>
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          maxWidth: `400px`,
-          marginBottom: `1rem`,
-          fontSize: `12px`,
-        }}
-      >
-        <span style={{ width: `80px` }}>Kenkeni</span>
-        <Space />
-        <Bell />
-        <Bell />
-        <Space />
-        <Open />
-        <Open />
-        <Space />
-        <Bell />
-        <Bell />
-        <Space />
-        <Open />
-        <Open />
-      </div>
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          maxWidth: `400px`,
-          marginBottom: `1rem`,
-          fontSize: `12px`,
-        }}
-      >
-        <span style={{ width: `80px` }}>Sangban</span>
-        <Open />
-        <Space />
-        <Bell />
-        <Space />
-        <Closed />
-        <Space />
-        <Closed />
-        <Space />
-        <Bell />
-        <Open />
-        <Space />
-        <Bell />
-      </div>
-      <div
-        style={{
-          display: `flex`,
-          alignItems: `center`,
-          maxWidth: `400px`,
-          marginBottom: `1rem`,
-          fontSize: `12px`,
-        }}
-      >
-        <span style={{ width: `80px` }}>Dununba</span>
-        <Open />
-        <Space />
-        <Open />
-        <Space />
-        <Bell />
-        <Space />
-        <Bell />
-        <Space />
-        <Open />
-        <Open />
-        <Space />
-        <Open />
-      </div>
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Row title="Kenkeni" nodes={[0, 1, 1, 0, 2, 2, 0, 1, 1, 0, 2, 2]} />
+    <Row title="Sangban" nodes={[2, 0, 1, 0, 3, 0, 3, 0, 1, 2, 0, 1]} />
+    <Row title="Dununba" nodes={[2, 0, 2, 0, 1, 0, 1, 0, 2, 2, 0, 2]} />
   </Layout>
 )
 
